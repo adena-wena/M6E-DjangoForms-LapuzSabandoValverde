572a0db9685b9f321b3d928f674fced5520e5d59
sysabando <123818746+sysabando@users.noreply.github.com>
2025-04-10 18:10:36 +0800

    Add files via upload

---
+++ Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect, get_object_or_404 # type: ignore
+from .models import WaterBottle, Supplier
+# Create your views here.
+
+
+def view_supplier(request):
+    supplier_objects = Supplier.objects.all()
+    return render(request, 'view_supplier.html', {'supplier' :supplier_objects})
+
+def view_bottles(request):
+    bottles_objects = WaterBottle.objects.all()
+    return render(request, 'view_bottles.html', {'bottles' :bottles_objects})
+
+def view_bottle_details(request, pk):
+    b = get_object_or_404(WaterBottle, pk=pk)
+    return render(request, 'view_bottle_details.html', {'b': b})
+
+def add_bottle(request):
+    if request.method == "POST":
+        sku = request.POST.get('sku')
+        brand = request.POST.get('brand')
+        cost = request.POST.get('cost')
+        size = request.POST.get('size')
+        mouth_size = request.POST.get('mouth_size')
+        color = request.POST.get('color')
+        supplied_by = request.POST.get('supplied_by')
+        current_qty = request.POST.get('current_qty')
+
+        supplier = Supplier.objects.get(id=supplied_by)
+
+        WaterBottle.objects.create(
+            sku=sku,
+            brand=brand,
+            cost=cost,
+            size=size,
+            mouth_size=mouth_size,
+            color=color,
+            supplier=supplier,
+            current_quantity=current_qty
+        )        
+        return redirect('view_bottles')
+    else:
+        supplier_objects = Supplier.objects.all()
+        return render(request, 'add_bottle.html', {'supplier': supplier_objects})
+
+def delete_bottle(request, pk):
+    WaterBottle.objects.filter(pk=pk).delete()
+    return redirect('view_bottles')
