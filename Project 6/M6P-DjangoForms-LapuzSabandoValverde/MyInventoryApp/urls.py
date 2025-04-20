5b639d6aa859d87731475d3254d83b6b84162e73
JacyV5 <jacy.l.valverde@student.ateneo.edu>
2025-04-20 15:14:08 +0800

    Update urls.py

--- Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/urls.py
+++ Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/urls.py
@@ -19,6 +19,8 @@ from django.urls import path # type: ignore
 from .import views
 
 urlpatterns = [
+    path('', views.base, name='base'),  
+    path('signup/', views.signup, name='signup'),
     path('view_supplier', views.view_supplier, name='view_supplier'),
     path('view_bottles', views.view_bottles, name='view_bottles'),
     path('view_bottle_details/<int:pk>/', views.view_bottle_details, name= 'view_bottle_details'),
