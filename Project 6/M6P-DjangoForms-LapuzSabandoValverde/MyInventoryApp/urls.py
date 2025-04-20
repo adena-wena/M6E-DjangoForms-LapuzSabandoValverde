ba52eacd6094ecb61c3939e31386158b54ee29b2
adena-wena <athena.lapuz@student.ateneo.edu>
2025-04-20 23:24:15 +0800

    Update urls.py

--- Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/urls.py
+++ Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('view_bottles', views.view_bottles, name='view_bottles'),
     path('view_bottle_details/<int:pk>/', views.view_bottle_details, name= 'view_bottle_details'),
     path('delete_bottle/<int:pk>/', views.delete_bottle, name='delete_bottle'),
+    path('manage_account/<int:pk>/', views.manage_account, name='manage_account'),
+    path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
     path('add_bottle', views.add_bottle, name='add_bottle'),
 ]
