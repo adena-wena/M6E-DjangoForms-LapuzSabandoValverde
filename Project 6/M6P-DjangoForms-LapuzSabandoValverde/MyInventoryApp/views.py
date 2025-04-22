0bbc6ebb316765093c5b3b4b2f6ca0b23327073b
JacyV5 <jacy.l.valverde@student.ateneo.edu>
2025-04-22 10:43:37 +0800

    Update views.py

--- Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/views.py
+++ Project 6/M6P-DjangoForms-LapuzSabandoValverde/MyInventoryApp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404 # type: ignore
 from .models import WaterBottle, Supplier, Account
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
